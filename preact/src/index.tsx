9cbf3db47ba9e7f9ae00ac84c4630af30ad78994
harsh vairagi <harshb7839@gmail.com>
2025-07-14 13:42:52 +0530

    fixed: height is overflowing.
    - more optimizations (now code will once check for template tag)
    - pre-added fa-icons class name added in every icon.
    - now color can use to add color.

--- preact/src/index.tsx
+++ preact/src/index.tsx
@@ -5,18 +5,19 @@ type FaParams = {
   className?: string
 }
 
+const engine = () => document.getElementById("@fa-icons/all")
+if (engine() === null) document.body.appendChild(Object.assign(document.createElement("template"),{id: "@fa-icons/all"}))
+
 export const Fa = ({icon, size = "1em", style, className}: FaParams) => {
   const id = `@fa-icons/${icon.name}`
   const appendChild = () => {
-    const engine = () => document.getElementById("@fa-icons/all")
-    if (engine() === null) document.body.appendChild(Object.assign(document.createElement("template"),{id: "@fa-icons/all"}))
     engine().insertAdjacentHTML("beforeend", `
     <svg hidden="true" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" style="display: none;">
     <symbol id="${id}" viewBox="${icon.viewBox}"><path d="${icon.d}"/></symbol>
     </svg>`)
   }
   if (document.getElementById(id) === null) appendChild()
-  return <svg style={style} width={size} className={className}>
-    <use href={`#${id}`}/>
+  return <svg style={{ width: size, height: size, ...style }} className={`fa-icons${className ? ` ${className}` : ''}`}>
+    <use fill="currentColor" href={`#${id}`}/>
   </svg>
 }
