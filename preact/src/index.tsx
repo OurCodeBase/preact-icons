04125f8c6f5b76a099daf956bae27e07491d0be6
harsh vairagi <harshb7839@gmail.com>
2025-07-18 20:54:57 +0530

    now Fa extends svg html tag (support every attributes).

--- preact/src/index.tsx
+++ preact/src/index.tsx
@@ -1,20 +1,19 @@
-type FaParams = {
+import { ComponentProps } from 'preact/compat';
+
+interface FaParams extends ComponentProps<'svg'> {
   icon: { name: string; viewBox: string; d: string },
   size?: string,
   style?: import('preact/compat').CSSProperties,
-  className?: string
 }
 
 const engine = () => document.getElementById('@fa-icons/all')
 if (engine() === null) document.body.appendChild(Object.assign(document.createElement('template'), { id: '@fa-icons/all' }))
 
-export const Fa = ({icon, size = '1em', style, className}: FaParams) => {
+export const Fa = ({icon, size = '1em', style, className, ...daemon}: FaParams) => {
   const id = `@fa-icons/${icon.name}`
-  const appendChild = () => {
+  if (document.getElementById(id) === null)
     engine().insertAdjacentHTML('beforeend', `<svg hidden="true" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" style="display: none;"><symbol id="${id}" viewBox="${icon.viewBox}"><path d="${icon.d}"/></symbol></svg>`)
-  }
-  if (document.getElementById(id) === null) appendChild()
-  return <svg style={{ width: size, height: size, verticalAlign: 'text-top', ...style }} className={`fa-icons${className ? ` ${className}` : ''}`}>
+  return <svg style={{ width: size, height: size, verticalAlign: 'text-top', ...style }} className={`fa-icons fa${icon.name}${className ? ` ${className}` : ''}`} {...daemon}>
     <use fill="currentColor" href={`#${id}`}/>
   </svg>
 }
