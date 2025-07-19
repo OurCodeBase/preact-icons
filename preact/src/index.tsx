da63db291879e5b0f1b46fc889e58d364cb02f2b
harsh vairagi <harshb7839@gmail.com>
2025-07-19 14:13:30 +0530

    added more optimizations.

--- preact/src/index.tsx
+++ preact/src/index.tsx
@@ -7,12 +7,12 @@ interface FaParams extends ComponentProps<'svg'> {
 }
 
 const engine = () => document.getElementById('@fa-icons/all')
-if (engine() === null) document.body.appendChild(Object.assign(document.createElement('template'), { id: '@fa-icons/all' }))
+if (engine() === null) document.body.insertAdjacentHTML('beforeend', '<svg id="@fa-icons/all" hidden="true" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" style="display: none;"/>')
 
 export const Fa = ({icon, size = '1em', style, className, ...daemon}: FaParams) => {
   const id = `@fa-icons/${icon.name}`
   if (document.getElementById(id) === null)
-    engine().insertAdjacentHTML('beforeend', `<svg hidden="true" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" style="display: none;"><symbol id="${id}" viewBox="${icon.viewBox}"><path d="${icon.d}"/></symbol></svg>`)
+    engine().insertAdjacentHTML('beforeend', `<symbol id="${id}" viewBox="${icon.viewBox}"><path d="${icon.d}"/></symbol>`)
   return <svg style={{ width: size, height: size, verticalAlign: '-0.125em', ...style }} className={`fa-icons fa-${icon.name}${className ? ` ${className}` : ''}`} {...daemon}>
     <use fill="currentColor" href={`#${id}`}/>
   </svg>
