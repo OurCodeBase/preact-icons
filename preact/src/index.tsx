5e88fa4ef0ae046b4640d1428834eee8ece18366
harsh vairagi <harshb7839@gmail.com>
2025-07-19 12:01:06 +0530

    icons will now align with texts.

--- preact/src/index.tsx
+++ preact/src/index.tsx
@@ -13,7 +13,7 @@ export const Fa = ({icon, size = '1em', style, className, ...daemon}: FaParams) 
   const id = `@fa-icons/${icon.name}`
   if (document.getElementById(id) === null)
     engine().insertAdjacentHTML('beforeend', `<svg hidden="true" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" style="display: none;"><symbol id="${id}" viewBox="${icon.viewBox}"><path d="${icon.d}"/></symbol></svg>`)
-  return <svg style={{ width: size, height: size, verticalAlign: 'text-top', ...style }} className={`fa-icons fa${icon.name}${className ? ` ${className}` : ''}`} {...daemon}>
+  return <svg style={{ width: size, height: size, verticalAlign: '-0.125em', ...style }} className={`fa-icons fa${icon.name}${className ? ` ${className}` : ''}`} {...daemon}>
     <use fill="currentColor" href={`#${id}`}/>
   </svg>
 }
